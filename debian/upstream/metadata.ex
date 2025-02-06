02ef88c3558317cd0b8121340770c3fd9484ae2b
difcsi <zoltan.m@tuta.io>
2025-02-06 13:50:10 +0000

    feat: initial debian packager

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+Bug-Database: https://github.com/stephenrkell/liballocs/issues
+Bug-Submit: https://github.com/stephenrkell/liballocs/issues/new
+# Changelog: https://github.com/stephenrkell/liballocs/blob/master/CHANGES
+# Documentation: https://github.com/stephenrkell/liballocs/wiki
+Repository-Browse: https://github.com/stephenrkell/liballocs
+Repository: https://github.com/stephenrkell/liballocs.git
